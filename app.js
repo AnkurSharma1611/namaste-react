5432806c2b864afc8eeffa73ddee7f8594a646f5
Ankur Sharma <ankursharma.1611@gmail.com>
2023-06-25 11:44:15 +0530

    episode 2

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
-/* 
-=> React just a javascript <=
-<div id="parent">
-  <div id="child">
-    <h1>I'am h1 tag</h1>
-    <h2>I'am h2 tag</h2>
-  </div>
-  <div id="child2">
-    <h1>I'am h1 tag</h1>
-    <h2>I'am h2 tag</h2>
-  </div>
-  
-</div>
-ReactElement(object) => HTML(Browser Understands)
- */
+import React from "react";
+import ReactDOM from "react-dom";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
+    React.createElement("h1", {}, "This is Namaste React"),
+    React.createElement("h2", {}, "by Ankur Sharma"),
   ]),
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I'm h1 tag"),
